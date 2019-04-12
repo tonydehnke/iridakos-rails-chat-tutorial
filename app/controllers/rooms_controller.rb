a48045379a2f7daf596e36f050d9a5e870bb0d06
Tony Dehnke <tdehnke@me.com>
2019-04-12 10:49:07 -0700

    Room new/edit

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,7 +1,51 @@
 class RoomsController < ApplicationController
+  # Loads:
+  # @rooms = all rooms
+  # @room = current room when applicable
+
+  before_action :load_entities
 
   def index
     @rooms = Room.all
+  end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new permitted_parameters
+    if @room.save
+      flash[:success] = "Room #{@room.name} was successfully created"
+      redirect_to rooms_path
+    else
+      flash[:error] = "Something went wrong"
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+      if @room.update_attributes(permitted_parameters)
+        flash[:success] = "Room #{@room.name} was successfully updated"
+        redirect_to rooms_path
+      else
+        flash[:error] = "Something went wrong"
+        render :new
+      end
+  end
+
+  protected
+
+  def load_entities
+    @rooms = Room.all
+    @room = Room.find(params[:id]) if params[:id]
+  end
+
+  def permitted_parameters
+    params.require(:room).permit(:name)
   end
   
 end
