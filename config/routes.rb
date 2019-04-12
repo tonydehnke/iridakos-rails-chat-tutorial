a0a3d1f1e5bd524a166b29f7cdc40af2d8e4febc
Tony Dehnke <tdehnke@me.com>
2019-04-11 17:01:48 -0700

    Setup Rooms & Messages resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  root controller: :rooms, action: :index
+
+  resources :room_messages
+  resources :rooms
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
